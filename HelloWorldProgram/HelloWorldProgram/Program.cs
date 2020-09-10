924a8929db6622f01b9146e2a9d733c55ae8f772
Danish Ali <50976119+Danish-Ali097@users.noreply.github.com>
2020-09-10 19:33:44 +0500

    lecture 1

---
+++ HelloWorldProgram/HelloWorldProgram/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // HelloWorld Program
+           // Console.Write("Hello World");
+
+            /* // Implicit type casting
+             int myInt = 9;
+             double myDouble = myInt;       // Automatic casting: int to double
+
+              Console.WriteLine(myInt);      // Outputs 9
+              Console.WriteLine(myDouble);   // Outputs 9
+              */
+              // Explicit type casting
+             /* double myDouble2 = 9.78;
+              int myInt2 = (int)myDouble2;    // Manual casting: double to int
+
+              Console.WriteLine(myDouble2);   // Outputs 9.78
+              Console.WriteLine(myInt2);      // Outputs 9
+            
+
+             char a = 'h';
+             char b = 'i';
+             string str = String.Concat(a, b);
+             Console.Write(str); */
+            Console.ReadKey();
+        }
+    }
+}
